1dbc854aa5ce21e26cab679633e530bd2b1bdb01
abdelrahman <abdelrahmank8128@gmail.com>
2024-10-20 17:38:45 +0300

    seconde try

--- index.js
+++ index.js
@@ -1,60 +1,23 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const mongoose = require('mongoose');
-const redis = require('redis');
+const http =require('http');
+const server=http.createServer();
+const {Server}=require('socket.io');
+const io=new Server(server);
 
-// Initialize Redis client
-const redisClient = redis.createClient();
-redisClient.connect();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const port=3700;
 
-mongoose.connect('mongodb+srv://abdelrahmank8128:000000000010Aa@cluster0.ogteq8x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+io.on('connection',(socket)=>{
 
-const DriverSchema = new mongoose.Schema({
-  name: String,
-  available: Boolean,
-  location: {
-    lat: Number,
-    lng: Number,
-  },
-  startTime: Number,
-  totalActiveTime: Number,
-});
+  socket.on(
+    "postion-change",(data)=>{
+      io.emit("positon-change",data);
 
-const Driver = mongoose.model('Driver', DriverSchema);
-
-// Real-time socket handling
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle driver location updates
-  socket.on('update_driver_location', async (data) => {
-    const { driverId, location } = data;
-
-    // Update location in DB
-    await Driver.findByIdAndUpdate(driverId, {
-      $set: { location: location },
     });
-
-    // Optionally store in Redis for faster access
-    await redisClient.hSet('driverLocations', driverId, JSON.stringify(location));
-
-    // Broadcast the location update to the admin
-    io.emit('driver_location_update', { driverId, location });
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    socket.on("disconnect",(data)=>{});
+     
+  
 });
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000');
+server.listen(port,()=>{
+  console.log(`listening on ${port}`);
 });
